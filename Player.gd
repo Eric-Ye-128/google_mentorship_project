050f9f63ca5d94c2476733972d798cbe369f45e7
Eric Ye <144462517+Eric-Ye-128@users.noreply.github.com>
2024-04-01 16:38:53 -0400

    Revert "transitions"

--- Player.gd
+++ Player.gd
@@ -10,14 +10,9 @@ var is_moving: bool = false
 var speed: int = 0
 var MAX_SPEED: int = 200
 
-var entered = false
-
 func _ready():
 	self.position = Vector2(40, 40)
 
-func _on_area_2d_body_entered(body):
-	entered = true
-	
 func _process(delta):
 	direction = Vector2()
 	speed = 0
@@ -27,13 +22,11 @@ func _process(delta):
 	elif Input.is_action_pressed("Down"): direction.y = 1
 	elif Input.is_action_pressed("Up"): direction.y = -1
 	
-	if not is_moving and direction != Vector2():
+	if not is_moving and direction != Vector2(): 
 		target_direction = direction.normalized()
-		if not world.is_on_ground(position, target_direction) and not world.is_enterance(position, target_direction):
+		if not world.is_on_ground(position, target_direction):
 			target_pos = position + target_direction * 16
 			is_moving = true
-		if world.is_enterance(position, target_direction):
-			get_tree().change_scene_to_file("res://World2.tscn")
 	elif is_moving:
 		speed = MAX_SPEED
 		velocity = speed * target_direction * delta
@@ -45,4 +38,6 @@ func _process(delta):
 			is_moving = false
 		
 		move_and_collide(velocity)
-	
+
+func _on_area_2d_body_entered(body):
+	print("collided")
